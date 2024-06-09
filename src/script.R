e65d5a80554e221f9d97f5788bef64a94503940d
Garland Xie <garlandxie@gmail.com>
2024-06-09 19:04:28 -0400

    Added data visualizations

--- src/script.R
+++ src/script.R
@@ -1,10 +1,9 @@
 # libraries -----
-library(ggplot2)
-library(dplyr)
-library(lme4)
-library(lmerTest)
-library(here)
-library(car)
+library(car)       # for testing ANOVA type II models 
+library(dplyr)     # for manipulating data  
+library(ggplot2)   # for visualizing data
+library(here)      # for creating relative file-paths
+library(emmeans)   # for calculating least-square means 
 
 # import ----
 
@@ -27,7 +26,7 @@ perc_viab_tidy <- perc_viab_raw %>%
 # sample size for percent seed viability between inside + outside environment
 perc_viab_raw %>%
   group_by(Treatment) %>%
-  summarize(sample_size = sum(!is.na(Perc_viability))) %>%
+  summarize(sample_size = sum(!is.na(Perc_viability))) 
 
 (plot_env <- perc_viab_raw %>%
 
@@ -133,10 +132,10 @@ plot(glm_viab_und)
 
 ## anova ----
 
-# use type II ANOVA adjusted sum of squares for the unbalanced design
+# use type III ANOVA adjusted sum of squares for the unbalanced design
 # would like to see global significance of main effects and interactions
 # prior to running pairwise comparisons 
-anova_viab <- car::Anova(glm_viab_und, type = "II")
+anova_viab <- car::Anova(glm_viab_und, type = "II", test.statistic = "F")
 
 # check reference grid for unbalanced sample sizes 
 ref_grid_viab <- emmeans::ref_grid(glm_viab_und)
@@ -157,40 +156,154 @@ viab_emm_mow_t <- emmeans(glm_viab, pairwise ~ Mow | Treatment)
 
 # mowing and phenophase
 emm_mow_pheno <- emmeans(glm_viab_und, ~ Mow*Pheno)
-pairs_mow_pheno <- emmeans::contrast(
+
+pheno_over_time <- emmeans::contrast(
+  emm_mow_pheno, 
+  interaction = c("poly", "del.eff")
+  )
+
+pheno_per_week <- emmeans::contrast(
   emm_mow_pheno, 
   interaction = c("consec", "poly")
-  )
+)
 
 # pheno and treatment
 emm_pheno_trt <- emmeans(glm_viab_und, ~ Pheno*Treatment)
-pairs_pheno_trt <- emmeans::contrast(
-  emm_pheno_trt, 
-  interaction = c("poly", "consec")
-)
+pairs_pheno_trt <- pairs(emm_pheno_trt) 
+pairs_pheno_trt_tidy <- pairs_pheno_trt %>%
+  as.data.frame() %>%
+  dplyr::filter(
+    contrast %in% c(
+      "P2 Inside - P2 Outside",
+      "P3 Inside - P3 Outside",
+      "P4 Inside - P4 Outside"
+    )
+  )
 
 # visualize the effects of mowing and phenophase
 # to confirm results provided by the interaction contrasts  
-perc_viab_tidy %>%
+(perc_viab_tidy %>%
   filter(!is.na(Mow)) %>%
-  ggplot(aes(x = Pheno, y = Perc_viability)) + 
+  ggplot(aes(x = Mow, y = Perc_viability)) + 
   geom_boxplot() + 
   geom_point(alpha = 0.1) + 
-  facet_wrap(~Mow) + 
+  facet_wrap(~Pheno) + 
   ylim(0,1) +
   labs(
     x = "Phenophase",
     y = "Seed Viability (%) per flower head") + 
   theme_bw()
+)
 
-perc_viab_tidy %>%
-  filter(!is.na(Mow)) %>%
+(perc_viab_tidy %>%
+    filter(!is.na(Mow)) %>%
+    ggplot(aes(x = Pheno, y = Perc_viability)) + 
+    geom_point(alpha = 0.1) + 
+    stat_summary(fun = "mean", colour = "red", geom = "line") + 
+    facet_wrap(~Mow) + 
+    ylim(0,1) +
+    labs(
+      x = "Phenophase",
+      y = "Seed Viability (%) per flower head") + 
+    theme_bw()
+)
+
+
+emm_mow_pheno_df <- emm_mow_pheno %>%
+  as.data.frame() %>%
+  mutate(
+    back_emmean = boot::inv.logit(emmean), 
+    back_emmean = round(back_emmean, digits = 2)
+    ) %>% 
+   dplyr::filter(Mow %in% c("Day 7", "Day 14", "Day 21")) %>%
+   mutate(
+    Mow = case_when(
+      Mow == "Day 7"  ~ "Week 1 (Days 1 -7)", 
+      Mow == "Day 14" ~ "Week 2 (Days 7 - 14)", 
+      Mow == "Day 21" ~ "Week 3 (Days 14 - 21)", 
+      TRUE ~ Mow),
+    Pheno = case_when(
+      Pheno == "P2" ~ "Flowering", 
+      Pheno == "P3" ~ "Flowering completed", 
+      Pheno == "P4" ~ "Seeds matured", 
+      TRUE ~ Pheno)
+   )
+
+# data visualization ----
+
+## interaction contrast - phenophase per week ----
+(plot_pheno_per_week <- perc_viab_tidy %>%
+  mutate(Mow = as.character(Mow)) %>%
+  filter(!is.na(Mow) & Mow %in% c("Day 7", "Day 14", "Day 21")) %>%
+  mutate(
+    Mow = case_when(
+      Mow == "Day 7"  ~ "Week 1 (Days 1 -7)", 
+      Mow == "Day 14" ~ "Week 2 (Days 7 - 14)", 
+      Mow == "Day 21" ~ "Week 3 (Days 14 - 21)", 
+     TRUE ~ Mow),
+    Mow = factor(Mow),
+    Pheno = case_when(
+      Pheno == "P2" ~ "Flowering", 
+      Pheno == "P3" ~ "Flowering completed", 
+      Pheno == "P4" ~ "Seeds matured", 
+      TRUE ~ Pheno
+    )
+  ) %>%
   ggplot(aes(x = Pheno, y = Perc_viability)) + 
-  geom_boxplot() + 
   geom_point(alpha = 0.1) + 
-  facet_wrap(~Treatment) + 
+  geom_point(
+    aes(x = Pheno, y = back_emmean), 
+    color = "red", 
+    size = 2.5, 
+    shape = "triangle", 
+    data = emm_mow_pheno_df
+      ) +  
+  facet_wrap(~Mow, nrow = 3, ncol = 1) + 
   ylim(0,1) +
   labs(
     x = "Phenophase",
     y = "Seed Viability (%) per flower head") + 
+  theme_bw() 
+)
+
+(plot_pheno_trt_int <- perc_viab_tidy %>%
+  dplyr::filter(!is.na(Pheno)) %>%
+  ggplot(aes(x = Treatment, y = Perc_viability)) +
+  geom_boxplot() + 
+  facet_wrap(~Pheno) + 
+  labs(
+    x = "Storage Environment", 
+    y = "Percent seed viability (%)"
+    ) + 
   theme_bw()
+)
+
+# save to disk -----
+
+anova_viab %>%
+  janitor::clean_names() %>%
+  tibble::rownames_to_column(var = "variables") %>%
+  mutate(pr_chisq = round(pr_f, digits = 2)) %>%
+  write.csv(
+  file = here("output", "anova_viab.csv"), 
+  row.names = FALSE
+)
+
+ggsave(
+  filename = here("output", "plot_pheno_per_week.png"), 
+  plot = plot_pheno_per_week, 
+  device = "png", 
+  units = "in", 
+  width = 4, 
+  height = 5
+)
+
+
+ggsave(
+  filename = here("output", "plot_pheno_trt_int.png"), 
+  plot = plot_pheno_trt_int, 
+  device = "png", 
+  units = "in", 
+  width = 7, 
+  height = 5
+)
