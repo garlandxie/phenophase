a5a7ef7eb7e055106406d3030f65a7745a646da1
Garland Xie <garlandxie@gmail.com>
2024-06-12 17:03:28 -0400

    Add data visualizations

--- src/lm_stem_flower_width.R
+++ src/lm_stem_flower_width.R
@@ -36,7 +36,7 @@ perc_viab_tidy <- perc_viab_raw %>%
 ## stem width ------------------------------------------------------------------
 
 # model fit
-lm_stem_pheno <- lm(Width_flower ~ Pheno, data = perc_viab_tidy)
+lm_stem_pheno <- lm(Width_stem ~ Pheno, data = perc_viab_tidy)
 plot(lm_stem_pheno)
 
 car::Anova(lm_stem_pheno, type = "II")
@@ -70,12 +70,35 @@ emm_stem_width_df <- emm_stem_width %>%
     upper_cl = round(upper_cl, digits = 2)
   ) 
 
-(plot_flower_width <- emm_stem_width_df %>%
-  ggplot(aes(x = pheno, y = emmean)) + 
-  geom_point() + 
-  geom_pointrange(aes(ymin = lower_cl, ymax = upper_cl)) + 
+(plot_stem_width <- perc_viab_tidy %>%
+  dplyr::filter(!is.na(Pheno)) %>%
+  ggplot(aes(x = Pheno, y = Width_stem)) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(aes(x = pheno, y = emmean), data = emm_stem_width_df) +
   ylim(0, 2) + 
   labs(x = "Phenophase", y = "Stem width (mm)") + 
   theme_bw()
 )
+
+## flower width ----------------------------------------------------------------
+
+emm_flower_width_df <- emm_flower_width %>%
+  as.data.frame() %>%
+  janitor::clean_names() %>%
+  mutate(
+    emmean = round(emmean, digits = 2),
+    lower_cl = round(lower_cl, digits = 2),
+    upper_cl = round(upper_cl, digits = 2)
+  ) 
+
+(plot_flower_width <- emm_flower_width_df %>%
+    ggplot(aes(x = pheno, y = emmean)) + 
+    geom_point() + 
+    geom_pointrange(aes(ymin = lower_cl, ymax = upper_cl)) + 
+    ylim(0, 0.5) + 
+    labs(x = "Phenophase", y = "Flower width (mm)") + 
+    theme_bw()
+)
+
+## multi-panel figure ----------------------------------------------------------
 
